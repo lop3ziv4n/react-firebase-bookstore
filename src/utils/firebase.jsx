77c94d7f0bcf5e5130c74bd17c3d4f5715f8c2ff
lop3ziv4n <ivan.leandro.lopez@gmail.com>
2022-12-14 21:18:51 -0300

    first commit

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -2,13 +2,13 @@ import {initializeApp} from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDad1q8TgTM4AWdACRvGpDURTibFtmYyKY",
-    authDomain: "react-firebase-bookstore.firebaseapp.com",
-    projectId: "react-firebase-bookstore",
-    storageBucket: "react-firebase-bookstore.appspot.com",
-    messagingSenderId: "1004429134389",
-    appId: "1:1004429134389:web:b11a370dc51c905bebbf7c",
-    measurementId: "G-4NLP3EYJJQ"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
